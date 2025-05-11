6dbb8a04be2a95f984da585454d7bf79862edd4b
Grace663-08 <sivakumarbaranidaran@gmail.com>
2025-05-11 19:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyN0DFucCJFvnoSRMIH+YVLb",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
